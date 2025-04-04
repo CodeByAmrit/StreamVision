61d07d2d498abc3d78671fc2672af41ddb18d03b
Amrit <35948782+CodeByAmrit@users.noreply.github.com>
2025-04-04 17:34:42 +0000

    checkauth fix

--- routes/userRouters.js
+++ routes/userRouters.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const checkAuth = require('../services/checkAuth');
+const checkAuth = require('../services/checkauth');
 const {getAllDvrs} = require("../controllers/dvrController");
 require('dotenv').config()
 
